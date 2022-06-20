7f889f3850c9029a61120a7772cf9d52d4e66c9c
CodeaInterns <107670562+CodeaInterns@users.noreply.github.com>
2022-06-20 12:10:27 +0530

    dropleft-content in header

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 				<div class="col-lg-10 col-md-8 text-center text-lg-right text-md-right">
 					<div class="header-top-info">
 						<a href="tel:+23-345-67890">Call Us : <span>+1 356-677-5555</span></a>
-						<a href="mailto:info@avantiriumsolutions.us" ><i class="fa fa-envelope mr-2"></i><span>info@avantiriumsolutions.us</span></a>
+						<a href="mailto:info@avantiriumsolutions.us"><i class="fa fa-envelope mr-2"></i><span>info@avantiriumsolutions.us</span></a>
 					</div>
 				</div>
 			</div>
@@ -29,26 +29,27 @@
 			</button>
 			<div class="collapse navbar-collapse text-center" id="navbarsExample09">
 				<ul class="navbar-nav ml-auto">
-				<li class="nav-item active">
-				<a class="nav-link" href="index.php"><strong>Home</strong> <span class="sr-only">(current)</span></a>
-				</li>
-				<li class="nav-item"><a class="nav-link" href="about.php"><strong>About Us</strong></a></li>
-				<div>
-					<div class="dropdown">
-						<a href="services.php" class="nav-link">
-							<li class="nav-item">
-							<strong>Services</strong>
+					<li class="nav-item active">
+						<a class="nav-link" href="index.php"><strong>Home</strong> <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item"><a class="nav-link" href="about.php"><strong>About Us</strong></a></li>
+					<li class="nav-item">
+						<div class="dropdown">
+							<a href="services.php" class="nav-link"><strong>Services</strong></a>
 							<div class="dropdown-content" style="text-align:left;">
-								<a href="services.php">Mortgage Services</a>
+								<div class="dropleft-1">
+									<a href="#">Mortgage Services&nbsp;&nbsp;></a>
+									<div class="dropleft-content-1">
+										<a href="services.php">Test</a>
+									</div>
+								</div>
 								<a href="#">QC Services</a>
 								<a href="#">More Services</a>
-							</div>      
-							</li>
-						</a>
-					</div>
-				</div> 
-				<li class="nav-item"><a class="nav-link" href="how-we-work.php"><strong>How We Work</strong></a></li>
-				<li class="nav-item"><a class="nav-link" href="contact.php"><strong>Contact</strong></a></li>
+							</div>
+						</div>
+					</li>
+					<li class="nav-item"><a class="nav-link" href="how-we-work.php"><strong>How We Work</strong></a></li>
+					<li class="nav-item"><a class="nav-link" href="contact.php"><strong>Contact</strong></a></li>
 				</ul>
 			</div>
 		</div>
--- services.php
+++ services.php
@@ -1,12 +1,13 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="megakit,business,company,agency,multipurpose,modern,bootstrap4">
-  
- <meta name="mortgage processing" content="Avantirium">
+
+  <meta name="mortgage processing" content="Avantirium">
 
   <title>Avantirium | Business Solutions</title>
 
@@ -22,120 +23,121 @@
 
   <!-- Main Stylesheet -->
   <link rel="stylesheet" href="css/style.css">
-  
+
   <link rel="shortcut icon" href="images/logo.png">
-  
+
 </head>
 
 <body>
 
-<?php include('header.php'); ?>
+  <?php include('header.php'); ?>
 
-<div class="main-wrapper ">
-<section class="page-title bg-3">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="block text-center">
-          <h1 class="text-capitalize mb-3 text-lg">Services</h1>
-          <ul class="list-inline">
-            <li class="list-inline-item"><a href="index.php" class="text-white">Home</a></li>
-            <li class="list-inline-item"><span class="text-white">/</span></li>
-            <li class="list-inline-item"><a href="#" class="text-white-50">Services</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-<section class="section blog-wrap bg-gray">
-  <div class="container">
-    <h1 class="section-title">Mortgage Services</h1>
-    <div class="row">
-      <div class="col-lg-4 col-md-4 mb-3">
-        <div class="blog-item">
-          <a href="services-1.php"><img src="images/services/1.jpg" alt="" class="img-fluid rounded"></a>
-          <div class="blog-item-content bg-white" style="padding: 10px 30px 30px 30px">
-            <h4 class="mt-3 mb-3"><a href="services-1.php">Mortgage Loan Services</a></h4>
-            <p class="mb-3">Our loan servicing services is designed to facilitate collection of interest, principal, and escrow payments from a borrower.</p>
-            <a href="services-1.php" class="btn btn-small btn-main btn-round-full" style="margin-top: 30px;">Learn More</a>
+  <div class="main-wrapper ">
+    <section class="page-title bg-3">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="block text-center">
+              <h1 class="text-capitalize mb-3 text-lg">Services</h1>
+              <ul class="list-inline">
+                <li class="list-inline-item"><a href="index.php" class="text-white">Home</a></li>
+                <li class="list-inline-item"><span class="text-white">/</span></li>
+                <li class="list-inline-item"><a href="#" class="text-white-50">Services</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
+    </section>
 
-      <div class="col-lg-4 col-md-4 mb-3">
-        <div class="blog-item">
-          <a href="services-2.php"><img src="images/blog/2.jpg" alt="" class="img-fluid rounded"></a>
+    <section class="section blog-wrap bg-gray">
+      <div class="container">
+        <h1 class="section-title">Mortgage Services</h1>
+        <div class="row">
+          <div class="col-lg-4 col-md-4 mb-3">
+            <div class="blog-item">
+              <a href="services-1.php"><img src="images/services/1.jpg" alt="" class="img-fluid rounded"></a>
+              <div class="blog-item-content bg-white" style="padding: 10px 30px 30px 30px">
+                <h4 class="mt-3 mb-3"><a href="services-1.php">Mortgage Loan Services</a></h4>
+                <p class="mb-3">Our loan servicing services is designed to facilitate collection of interest, principal, and escrow payments from a borrower.</p>
+                <a href="services-1.php" class="btn btn-small btn-main btn-round-full" style="margin-top: 30px;">Learn More</a>
+              </div>
+            </div>
+          </div>
 
-          <div class="blog-item-content bg-white" style="padding: 10px 30px 30px 30px">
-              
+          <div class="col-lg-4 col-md-4 mb-3">
+            <div class="blog-item">
+              <a href="services-2.php"><img src="images/blog/2.jpg" alt="" class="img-fluid rounded"></a>
 
-            <h4 class="mt-3 mb-3"><a href="services-2.php">Mortgage Pre-processing Services</a></h4>
-            <p class="mb-3">Aventirium specializes in providing lenders and credit unions a comprehensive pre-processing support solution.</p>
+              <div class="blog-item-content bg-white" style="padding: 10px 30px 30px 30px">
 
-            <a href="services-2.php" class="btn btn-small btn-main btn-round-full">Learn More</a>
+
+                <h4 class="mt-3 mb-3"><a href="services-2.php">Mortgage Pre-processing Services</a></h4>
+                <p class="mb-3">Aventirium specializes in providing lenders and credit unions a comprehensive pre-processing support solution.</p>
+
+                <a href="services-2.php" class="btn btn-small btn-main btn-round-full">Learn More</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-4 mb-3">
+            <div class="blog-item">
+              <a href="services-3.php"><img src="images/blog/3.jpg" alt="" class="img-fluid rounded"></a>
+
+              <div class="blog-item-content bg-white" style="padding: 10px 30px 30px 30px">
+                <h4 class="mt-3 mb-3"><a href="services-3.php">Mortgage Processing Services</a></h4>
+                <p class="mb-3">Meeting document compliance is mandatory for meeting growing
+                  requirements of proof for regulatory compliance.</p>
+
+                <a href="services-3.php" class="btn btn-small btn-main btn-round-full">Learn More</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 mb-3">
+
+            <div class="blog-item">
+              <a href="services-4.php"><img src="images/blog/4.jpg" alt="" class="img-fluid rounded"></a>
+
+              <div class="blog-item-content bg-white" style="padding: 10px 30px 30px 30px">
+
+
+                <h4 class="mt-3 mb-3"><a href="services-4.php">Mortgage Underwriting Services</a></h4>
+                <p class="mb-3">We leverage various automated underwriting systems to deliver real time underwriting decision about the customer to the lender.</p>
+
+                <a href="services-4.php" class="btn btn-small btn-main btn-round-full">Learn More</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+    </section>
 
-      <div class="col-lg-4 col-md-4 mb-3">
-        <div class="blog-item">
-          <a href="services-3.php"><img src="images/blog/3.jpg" alt="" class="img-fluid rounded"></a>
-
-          <div class="blog-item-content bg-white" style="padding: 10px 30px 30px 30px">
-            <h4 class="mt-3 mb-3"><a href="services-3.php">Mortgage Processing Services</a></h4>
-            <p class="mb-3">Meeting document compliance is mandatory for meeting growing 
-              requirements of proof for regulatory compliance.</p>
-
-            <a href="services-3.php" class="btn btn-small btn-main btn-round-full">Learn More</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-4 mb-3">
-        
-        <div class="blog-item">
-          <a href="services-4.php"><img src="images/blog/4.jpg" alt="" class="img-fluid rounded"></a>
-
-          <div class="blog-item-content bg-white" style="padding: 10px 30px 30px 30px">
-              
-
-            <h4 class="mt-3 mb-3"><a href="services-4.php">Mortgage Underwriting Services</a></h4>
-            <p class="mb-3">We leverage various automated underwriting systems to deliver real time underwriting decision about the customer to the lender.</p>
-
-            <a href="services-4.php" class="btn btn-small btn-main btn-round-full">Learn More</a>
-          </div>
-        </div>
-      </div>
-    </div>   
-  </div>
-</section>
-
-<?php include('footer.php'); ?>
+    <?php include('footer.php'); ?>
 
 <!-- 
 Essential Scripts
 =====================================-->
 
-<!-- Main jQuery -->
-<script src="plugins/jquery/jquery.js"></script>
-<script src="js/contact.js"></script>
-<!-- Bootstrap 4.3.1 -->
-<script src="plugins/bootstrap/js/popper.js"></script>
-<script src="plugins/bootstrap/js/bootstrap.min.js"></script>
-<!--  Magnific Popup-->
-<script src="plugins/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
-<!-- Slick Slider -->
-<script src="plugins/slick-carousel/slick/slick.min.js"></script>
-<!-- Counterup -->
-<script src="plugins/counterup/jquery.waypoints.min.js"></script>
-<script src="plugins/counterup/jquery.counterup.min.js"></script>
+    <!-- Main jQuery -->
+    <script src="plugins/jquery/jquery.js"></script>
+    <script src="js/contact.js"></script>
+    <!-- Bootstrap 4.3.1 -->
+    <script src="plugins/bootstrap/js/popper.js"></script>
+    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!--  Magnific Popup-->
+    <script src="plugins/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
+    <!-- Slick Slider -->
+    <script src="plugins/slick-carousel/slick/slick.min.js"></script>
+    <!-- Counterup -->
+    <script src="plugins/counterup/jquery.waypoints.min.js"></script>
+    <script src="plugins/counterup/jquery.counterup.min.js"></script>
 
-<!-- Google Map -->
-<script src="plugins/google-map/map.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>    
+    <!-- Google Map -->
+    <script src="plugins/google-map/map.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAkeLMlsiwzp6b3Gnaxd86lvakimwGA6UA&callback=initMap"></script>
 
-<script src="js/script.js"></script>
+    <script src="js/script.js"></script>
 
-  </body>
-  </html>
+</body>
+
+</html>
