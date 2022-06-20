3f3cdc4495a43888f1064a3d1301ddb9c4aebfb9
CodeaInterns <107670562+CodeaInterns@users.noreply.github.com>
2022-06-20 16:19:52 +0530

    qc bg img -nitis

--- services-qc-1.php
+++ services-qc-1.php
@@ -31,7 +31,7 @@
 	<?php include('header.php'); ?>
 
 	<div class="main-wrapper ">
-		<section class="page-title bg-services-1">
+		<section class="page-title bg-qc-1">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
