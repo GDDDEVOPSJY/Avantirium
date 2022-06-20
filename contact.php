dec5d545cf7ee49c97e023eea042e1695ecf2a22
CodeaInterns <107670562+CodeaInterns@users.noreply.github.com>
2022-06-20 14:13:23 +0530

    contact.php -nitis

--- contact.php
+++ contact.php
@@ -116,8 +116,7 @@
 
             </div>
         </div> -->
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3471.1591198635206!2d-95.43327568508415!3d29.54086008206832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640ecfe73abf873%3A0xc2fe2478bed00b1e!2s13520%20Mason%20Canyon%20Ln%2C%20Pearland%2C%20TX%2077584%2C%20USA!5e0!3m2!1sen!2sin!4v1655710537212!5m2!1sen!2sin" 
-            width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3471.1591198635206!2d-95.43327568508415!3d29.54086008206832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8640ecfe73abf873%3A0xc2fe2478bed00b1e!2s13520%20Mason%20Canyon%20Ln%2C%20Pearland%2C%20TX%2077584%2C%20USA!5e0!3m2!1sen!2sin!4v1655710537212!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
         <?php include('footer.php'); ?>
 
