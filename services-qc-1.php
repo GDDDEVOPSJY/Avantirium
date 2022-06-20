df505409102acf457ad970a7fcd2fe48e28d1f77
CodeaInterns <107670562+CodeaInterns@users.noreply.github.com>
2022-06-20 12:45:10 +0530

    qc 2 and 3

--- services-qc-1.php
+++ services-qc-1.php
@@ -35,13 +35,16 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="block text-center">
-                        <h1 class="text-capitalize mb-4 text-lg">Mortgage Loan Services</h1>
+                        <h1 class="text-capitalize mb-4 text-lg">Mortgage Post Closing Audit Services</h1>
                         <ul class="list-inline">
                             <li class="list-inline-item"><a href="index.php" class="text-white">Home</a></li>
                             <li class="list-inline-item"><span class="text-white">/</span></li>
                             <li class="list-inline-item"><a href="services.php" class="text-white">Services</a></li>
                             <li class="list-inline-item"><span class="text-white">/</span></li>
-                            <li class="list-inline-item"><a href="#" class="text-white-50">Mortgage Loan Services</a></li>
+                            <li class="list-inline-item"><a href="#" class="text-white-50">Mortgage QC Services</a></li>
+                            <li class="list-inline-item"><span class="text-white">/</span></li>
+                            <li class="list-inline-item"><a href="#" class="text-white-50">Mortgage Post Closing Audit Services</a></li>
+
                         </ul>
                     </div>
                 </div>
@@ -57,6 +60,91 @@
         </div>
     </section>
 </div>
+
+
+<!--Main Part-->
+
+<section class="section service border-top bg-gray">
+	<div class="container">
+		<div class="row">
+		
+		
+		<h2>Mortgage Post Closing Audit Services</h2>
+							
+		
+        <p>We have more than a decade of experience in providing lenders with robust back office support for post-closing mortgage audits. Our services comprise of meticulous quality analysis and review processes that ensure that your mortgage loan approval process adheres to all the regulatory requirements of government-sponsored enterprises. As a premium Mortgage quality control company, our mortgage audit after closing services enables you perform vital tasks like loan default reviews, mortgage file review and denied loan reviews and compile an in-depth oversight of the client files</p>
+        <p>Our services comprise industry standard procedures that conduct an extensive audit based on a predetermined mortgage post-closing audit checklist. Our systematic approach to post-closing audits enables us to identify discrepancies with the loan information and flag it at the right time as well as initiate a re-underwriting process, if need be. Our clients prefer to outsource post-closing quality control services because of our established process and diverse experience in discharging this critical function.
+        </p>
+
+        <h3>Our Comprehensive Post-Closing Mortgage Audit Services</h3><br/><br/>
+				<div class="panel-group col-12">
+		
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+							<div class="panel-heading">
+								<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse1"> 
+								  > QC audit and Review of all Loan Documents</a></h4>
+							</div>
+							<div id="collapse1" class="collapse">
+								<div class="panel-body"><p class="text-justify">We adopt a checklist-based approach to review all the loan documents. These include documents such as, property information, appraisal information and the underwriting report etc. Our experts scan these documents to uncover misrepresentations and errors. With a meticulous level of review we aim to ease your workload and help you focus on other specific revenue generating tasks. Besides guaranteeing accuracy and efficiency, we also help you reduce the operational costs involved with the post-closing QC audit process.</p></br>
+									
+								</div>
+							</div>
+						</div>
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+						  <div class="panel-heading">
+							<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse2"> 
+							  > Risk and Data Integrity Analysis</a></h4>
+						  </div>
+						  <div id="collapse2" class="collapse">
+							<div class="panel-body">
+							  <p class="text-justify">We understand that the smallest of mistakes in recording data can lead to serious legal consequences. To help you avert such possibility, we have developed a multi-tier document verification system to ensure there is no compromise with data integrity. In case of any discrepancies, we flag it and recommend a review so that flawed assumptions can be rectified.</p></br>
+							</div>
+						  </div>
+						</div>
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+						  <div class="panel-heading">
+							<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse3"> 
+							  > Full compliance Review</a></h4>
+						  </div>
+						  <div id="collapse3" class="collapse">
+							<div class="panel-body">
+							  <p class="text-justify">As a premium mortgage company, we keep ourselves abreast of all the latest regulatory changes introduced by the compliance bodies. We make it a point to optimize our services according to these changes. It is because of this strict adherence that our post-closing mortgage audits are always in line with updated regulatory changes. This will ensure you always remain on the right side of compliance requirements.</p></br>
+							</div>
+						  </div>
+						</div>
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+						  <div class="panel-heading">
+							<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse4"> 
+							  > Credit Analysis</a></h4>
+						  </div>
+						  <div id="collapse4" class="collapse">
+							<div class="panel-body">
+							  <p class="text-justify">We document and generate reports on the individual profile of the client after a thorough assessment of their creditworthiness. Our approach to credit analysis greatly brings down your risk associated with the mortgage loan approval. The accuracy of our analysis will eliminate the chances of on-boarding a bad loan.</p></br>
+							</div>
+						  </div>
+						</div>
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+						  <div class="panel-heading">
+							<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse5"> 
+							  > Asset Re-Verification and Tax Transcripts</a></h4>
+						  </div>
+						  <div id="collapse5" class="collapse">
+							<div class="panel-body">
+							  <p class="text-justify">We verify the value of the collateral in detail to ensure that the loan amount that is being funded is not more than the value of the asset. We undertake the review of all taxes filed on the property to ensure that you do not have to bear the costs of any outstanding taxes on the asset. Our detailed approach to mortgage post-closing quality audit protects you from incurring unnecessary costs with the loan approval process.</p></br>
+							</div>
+						  </div>
+						</div>
+						
+					<div class="col-0 col-lg-2"></div>
+					<div class="col-12 col-lg-8">
+						<img class="col-12 pl-0" src="#" alt="">
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</section>
+
 
 <?php include('footer.php'); ?>
 
