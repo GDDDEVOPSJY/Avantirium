b16677b83248690fecdcda10fbea5cb860ed5564
CodeaInterns <107670562+CodeaInterns@users.noreply.github.com>
2022-06-20 13:12:34 +0530

    More 1 and 2

--- more-services-2(MI).php
+++ more-services-2(MI).php
@@ -35,13 +35,15 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="block text-center">
-                        <h1 class="text-capitalize mb-4 text-lg">Mortgage Loan Services</h1>
+                        <h1 class="text-capitalize mb-4 text-lg">Mortgage Indexing Services</h1>
                         <ul class="list-inline">
                             <li class="list-inline-item"><a href="index.php" class="text-white">Home</a></li>
                             <li class="list-inline-item"><span class="text-white">/</span></li>
                             <li class="list-inline-item"><a href="services.php" class="text-white">Services</a></li>
                             <li class="list-inline-item"><span class="text-white">/</span></li>
-                            <li class="list-inline-item"><a href="#" class="text-white-50">Mortgage Loan Services</a></li>
+                            <li class="list-inline-item"><a href="services.php" class="text-white">More Services</a></li>
+                            <li class="list-inline-item"><span class="text-white">/</span></li>
+                            <li class="list-inline-item"><a href="#" class="text-white-50">Mortgage Indexing Services</a></li>
                         </ul>
                     </div>
                 </div>
@@ -57,6 +59,96 @@
         </div>
     </section>
 </div>
+
+
+<!--Main Part-->
+
+<section class="section service border-top bg-gray">
+	<div class="container">
+		<div class="row">
+		
+		
+		<h2>Mortgage Indexing Services</h2>
+							
+		
+        <p>We have more than a decade of experience in providing mortgage document indexing and data extraction services for Mortgage Lenders. We leverage our vast experience in the mortgage industry and combine it with modern day technology like OCR and single document classification taxonomy system to provide lenders with a single stop solution to handle all document indexing needs. Our services enable you to digitize, store and track loan related files from loan origination to loan modifications stage. As a premier mortgage indexing services company, we enable you to focus on core business functions by ensuring data security and accuracy with all your business-critical documents.</p>
+        <p>Our indexing services are designed to help you reduce file storage space and improve process related workflows. Outsource indexing process in mortgage to us for an all-inclusive document management solution.</p>
+
+        <!-- <br/><br/> -->
+        <h3>Our Comprehensive Indexing Process In Mortgages</h3><br/><br/>
+				<div class="panel-group col-12">
+		
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+							<div class="panel-heading">
+								<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse1"> 
+								  > Forms Processing</a></h4>
+							</div>
+							<div id="collapse1" class="collapse">
+								<div class="panel-body"><p class="text-justify">The mortgage loan approval process begins with loan applications. Once the customer has filled out form 1003, our team of mortgage experts bank on our document management systems to digitize them. All supporting information like W-2 forms, pay stubs and credit check reports are categorized according to the profile of the customer and maintained. A systematic mortgage data capture process improves the pace and quality of your mortgage operations. The conversion of hard copies to soft copies makes your process more transparent and compliant.</p><br>
+									
+								</div>
+							</div>
+						</div>
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+						  <div class="panel-heading">
+							<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse2"> 
+							  > Document Indexing and Naming</a></h4>
+						  </div>
+						  <div id="collapse2" class="collapse">
+							<div class="panel-body">
+							  <p class="text-justify">The creation of a guide for the naming of business documents is the key to achieving greater levels of accuracy in document indexing. We use naming conventions like customer name, property information, credit check status, loan approval status and servicing status to classify customer data accordingly. With the use of well-defined indexing and naming conventions, we make the process of document classification, categorization and maintenance seamless. Our team assigned to outsource mortgage indexing services and data extraction ensure there are no missing links in your need to access mortgage loan related information on demand.</p><br>
+							</div>
+						  </div>
+						</div>
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+						  <div class="panel-heading">
+							<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse3"> 
+							  > Automated OCR and Data Capture</a></h4>
+						  </div>
+						  <div id="collapse3" class="collapse">
+							<div class="panel-body">
+							  <p class="text-justify">We leverage our in-built software tools to automate the entire indexing process and mortgage data extraction services. The tool comes with advanced features like Optical Character Recognition (OCR), which automatically extracts readable texts from documents. With the use of complex features we classify key index information like document number and make the process of data capture streamlined and well defined. Our automated indexing services help reduce your operational costs without compromising on the quality of the data that is being boarded. We scrutinize your entire business process in detail and identify areas for reducing manual intervention in the document digitization process.</p><br>
+							</div>
+						  </div>
+						</div>
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+						  <div class="panel-heading">
+							<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse4"> 
+							  > Making Documents Editable and Searchable</a></h4>
+						  </div>
+						  <div id="collapse4" class="collapse">
+							<div class="panel-body">
+							  <p class="text-justify">As an experienced mortgage indexing services company, we augment our data classification and indexing services with applications that have user interfaces containing advanced search features. Our mortgage document indexing services assure you of an error-free access to all your customer data. You can refine your search to pull out specific information about a customer’ profile. We integrate custom features that allow users to make and track changes made to the customer information accurately and with complete accountability. We bank on the latest document management systems and CRM tools to create an effective solution for retrieving business-critical information on demand.</p><br>
+							</div>
+						  </div>
+						</div>
+						<div class="panel panel-default" style="padding: 0 0 20px 0;" id="parnt">
+						  <div class="panel-heading">
+							<h4 class="panel-title"><a data-toggle="collapse" data-parent="#parnt" href="#collapse5"> 
+							  > Document Summary Generation</a></h4>
+						  </div>
+						  <div id="collapse5" class="collapse">
+							<div class="panel-body">
+							  <p class="text-justify">With the document summary generation, you get a bird’s eye view of all customer mortgage loans in various stages of approval. Our team of mortgage experts prepare a thoroughly verified summary of customer profiles. Such a meticulous approach will help in accelerating the process of loan closure and uncovers insights to improve your business process. We also prepare periodic reports in a summarized format to help you manage information that needs constant updating such as payment history, loan modification information and more.
+
+                        </p><br>
+							</div>
+						  </div>
+						</div>
+
+                        
+						
+					<div class="col-0 col-lg-2"></div>
+					<div class="col-12 col-lg-8">
+						<img class="col-12 pl-0" src="#" alt="">
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</section>
+
+
 
 <?php include('footer.php'); ?>
 
