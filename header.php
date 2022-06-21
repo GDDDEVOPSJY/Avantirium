551d61a3c6797d01e65ec90fe0f089017d8168e9
shahanas84 <107673615+shahanas84@users.noreply.github.com>
2022-06-21 16:40:32 +0530

    services

--- header.php
+++ header.php
@@ -1,3 +1,4 @@
+
 <header class="navigation">
 	<div class="header-top ">
 		<div class="container">
@@ -36,38 +37,57 @@
 					<li class="nav-item"><a class="nav-link" href="about.php"><strong>About Us</strong></a></li>
 					<li class="nav-item">
 						<div class="dropdown">
-							<a href="services.php" class="nav-link"><strong>Services</strong></a>
-							<div class="dropdown-content" style="text-align:left;">
-								<div class="dropleft-1">
-									<a href="services.php#Mortgage Services">Mortgage Services</a>
-									<div class="dropleft-content-1">
-										<a href="mortgage-loan-services.php">Loan Services</a>
-										<a href="mortgage-preprocessing-services.php">Pre-proccessing Services</a>
-										<a href="mortgage-processing-services.php">Processing Services</a>
-										<a href="mortgage-underwriting-services.php">Underwriting Services</a>
-										<a href="mortgage-closing-support-services.php">Closing Support Services</a>
-										<a href="mortgage-loan-modification-and-origination-services.php">Loan Modification And Origination Services</a>
-									</div>
-								</div>
-								<div class="dropleft-1">
-									<a href="services.php#QC Services">QC Services</a>
-									<div class="dropleft-content-1">
-										<a href="post-closing-audit-services.php">Post Closing Audit Services</a>
-										<a href="prefund-qc-audit-services.php">Prefund QC Audit Services</a>
-										<a href="underwriting-qc-audit-services.php">Underwriting QC Audit Services</a>
-									</div>
-								</div>
-								<div class="dropleft-1">
-									<a href="services.php#More Services">More Services</a>
-									<div class="dropleft-content-1">
-										<a href="loan-boarding-services.php">Loan Boarding Services</a>
-										<a href="mortgage-indexing-services.php">Mortgage Indexing Services</a>
-										<a href="reo-services.php">REO Services</a>
-										<a href="mortgage-loan-services.php">Title Support Services</a>
-									</div>
-								</div>
+							<a href="#" class="nav-link" onclick="document.getElementById('id01').style.display='block'"><strong>Services</strong></a>
+
+					<div id="id01" class="modal">
+					<span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+					<div class="services-container">
+						<div class="row">
+						<div class="col-4 text-center">
+							<h4 class="headings">Mortgage services</h4>
+							<a class="options" href="mortgage-loan-services.php">Loan Services</a><br>
+							<a class="options" href="mortgage-preprocessing-services.php">Preprocessing Services</a><br>
+							<a class="options" href="mortgage-processing-services.php">Processing Services</a><br>
+							<a  class="options" href="mortgage-underwriting-services.php">Underwriting Services</a><br>
+							<a class="options"  href="mortgage-closing-support-services.php">Closing Support Services</a><br>
+							<a class="options" href="mortgage-loan-modification-and-organisation-services.php">Loan Modification and Organisation Services</a><br>
+							</div>
+							<div class="col-4 text-center">
+							<h4 class="headings">QC services</h4>
+								<a class="options" href="post-closing-and-it-services.php">Post closing and IT Services</a><br>
+								<a class="options" href="prefund-qc-audit-services.php">Prefund QC audit Services</a><br>
+								<a class="options" href="underwriting-qc-and-it-services.php">Underwriting QC and IT Services</a><br>
+							
+							</div>
+							<div class="col-4 text-center">
+							<h4 class="headings">More Services</h4>
+								<a class="options" href="loan-boarding-services.php">Loan boarding Services</a><br>
+								<a class="options" href="mortage-indexing-services.php">Mortage indexing Services</a><br>
+								<a class="options" href="reo-services.php">Reo services</a><br>
+								<a class="options" href="title-support-services.php">Title support Services</a><br>
+							
 							</div>
 						</div>
+
+						</div>
+
+						
+
+					</div>
+					</div>
+
+					<script>
+					// Get the modal
+					var modal = document.getElementById('id01');
+
+					// When the user clicks anywhere outside of the modal, close it
+					window.onclick = function(event) {
+					if (event.target == modal) {
+						modal.style.display = "none";
+					}
+					}
+					</script>
+
 					</li>
 					<li class="nav-item"><a class="nav-link" href="how-we-work.php"><strong>How We Work</strong></a></li>
 					<li class="nav-item"><a class="nav-link" href="contact.php"><strong>Contact</strong></a></li>
