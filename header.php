fd365528e2f7a249ce71ef02af3d6052b912918a
CodeaInterns <107670562+CodeaInterns@users.noreply.github.com>
2022-06-23 10:18:09 +0530

    nitis

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
 					<img src="images/logo.png" width="60%" />
 				</a>
 				<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="fa fa-bars"></span>
+					<span class="fa fa-bars" style="color: black!important;"></span>
 				</button>
 
 				<div class="collapse navbar-collapse text-center" id="navbarsExample09">
@@ -38,82 +38,78 @@
 							<a class="nav-link" href="about.php"><strong>About Us</strong></a>
 						</li>
 						<li class="nav-item">
-							<div class="dropdown">
-								<a href="#" class="nav-link" onclick="document.getElementById('service-modal').style.display='block'"><strong>Services</strong></a>
+							<a href="#" class="nav-link" onclick="document.getElementById('service-modal').style.display='block'"><strong>Services</strong></a>
 
-								<div id="service-modal" class="modal">
-									<div class="container services row">
-										<div class="card col-3">
-											<div class="face face1">
+							<div id="service-modal" class="modal">
+								<div class="container services row">
+									<div class="card col-3 m-3">
+										<div class="row">
+											<div class="face face1 col-12">
 												<div class="content">
-													<a href="services.php#Mortgage Services"><img src="images/icons/header-services-1.png">
+													<a href="services.php#Mortgage Services">
+														<img src="images/icons/header-services-1-light.png" style="width: 60%; aspect-ratio: 1/1;">
 														<h3>Mortgage services</h3>
 													</a>
 												</div>
 											</div>
-											<div class="face face2">
+											<div class="face face2 col-12">
 												<div class="content half">
 													<a class="options" href="mortgage-loan-services.php">Mortgage Loan Services</a><br>
 													<a class="options" href="mortgage-preprocessing-services.php">Mortgage Preprocessing Services</a><br>
 													<a class="options" href="mortgage-processing-services.php">Mortgage Processing Services</a><br>
 												</div>
 												<div class="content half">
-													<a class="options" href="mortgage-underwriting-services.php">Mortgage Underwriting Services</a><br>
+													<a class="options" href="mortgage-underwriting-services.php">Underwriting Services</a><br>
 													<a class="options" href="mortgage-closing-support-services.php">Mortgage Closing Support Services</a><br>
 													<a class="options" href="mortgage-loan-modification-and-origination-services.php">Loan Modification and Origination Services</a><br>
 												</div>
 											</div>
 										</div>
-										<div class="card col-3">
-											<div class="face face1">
+									</div>
+									<div class="card col-3 m-3">
+										<div class="row">
+											<div class="face face1 col-12">
 												<div class="content">
-													<a href="services.php#QC Services"><img src="images/icons/header-services-2.png">
+													<a href="services.php#QC Services">
+														<img src="images/icons/header-services-2-light.png" style="width: 60%; aspect-ratio: 1/1;">
 														<h3>QC services</h3>
 													</a>
 												</div>
 											</div>
-											<div class="face face2">
+											<div class="face face2 col-12">
 												<div class="content">
-													<a class="options medium" href="post-closing-and-it-services.php">Post Closing and IT Services</a><br>
-													<a class="options medium" href="prefund-qc-audit-services.php">Prefund QC audit Services</a><br>
-													<a class="options medium" href="underwriting-qc-and-it-services.php">Underwriting QC and IT Services</a><br>
-												</div>
-											</div>
-										</div>
-										<div class="card col-3">
-											<div class="face face1">
-												<div class="content">
-													<a href="services.php#More Services"><img src="images/icons/header-services-3.png" height=60px width=60px>
-														<h3>More Services</h3>
-													</a>
-												</div>
-											</div>
-											<div class="face face2">
-												<div class="content">
-													<a class="options medium " href="loan-boarding-services.php">Loan boarding Services</a><br>
-													<a class="options medium" href="mortage-indexing-services.php">Mortage indexing Services</a><br>
-													<a class="options medium " href="reo-services.php">Reo services</a><br>
-													<a class="options medium" href="title-support-services.php">Title support Services</a><br>
+													<a class="options medium" href="mortgage-post-closing-audit-services.php">Mortgage Post Closing Audit Services</a><br>
+													<a class="options medium" href="mortgage-prefund-qc-audit-services.php">Mortgage Prefund QC Audit Services</a><br>
+													<a class="options medium" href="mortgage-underwriting-qc-services.php">Mortgage Underwriting QC Services</a><br>
 												</div>
 											</div>
 										</div>
 									</div>
-
-
-
+									<div class="card col-3 m-3">
+										<div class="row">
+											<div class="face face1 col-12">
+												<div class="content">
+													<a href="services.php#More Services">
+														<img src="images/icons/header-services-3-light.png" style="width: 45%; aspect-ratio: 1/1;">
+														<h3>More Services</h3>
+													</a>
+												</div>
+											</div>
+											<div class="face face2 col-12">
+												<div class="content">
+													<a class="options medium " href="loan-boarding-services.php">Loan Boarding Services</a><br>
+													<a class="options medium" href="mortgage-indexing-services.php">Mortgage Indexing Services</a><br>
+													<a class="options medium " href="reo-services.php">REO services</a><br>
+													<a class="options medium" href="title-support-services.php">Title Support Services</a><br>
+												</div>
+											</div>
+										</div>
+									</div>
 								</div>
-							</div>
-							<script>
-								// Get the modal
-								var modal = document.getElementById('service-modal');
 
-								// When the user clicks anywhere outside of the modal, close it
-								window.onclick = function(event) {
-									if (event.target == modal) {
-										modal.style.display = "none";
-									}
-								}
-							</script>
+
+
+							</div>
 						</li>
 						<li class="nav-item">
 							<a class="nav-link" href="how-we-work.php"><strong>How We Work</strong></a>
@@ -124,6 +120,17 @@
 					</ul>
 				</div>
 			</div>
+			<script>
+				// Get the modal
+				var modal = document.getElementById('service-modal');
+
+				// When the user clicks anywhere outside of the modal, close it
+				window.onclick = function(event) {
+					if (event.target == modal) {
+						modal.style.display = "none";
+					}
+				}
+			</script>
 		</nav>
 	</header>
 </div>
