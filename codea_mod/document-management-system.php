318d86574f1b16abcdcca41cb16de5d587444ae6
rakoon-cloud <88817543+rakoon-cloud@users.noreply.github.com>
2022-06-29 11:20:43 +0530

    d

--- codea_mod/document-management-system.php
+++ codea_mod/document-management-system.php
@@ -130,13 +130,13 @@
                 <div class="col-md-8 text-justify">
                     <ul style="padding-left:17px;">
                         <li>Systematically organize documents in a folder and subf older hierarchy</li>
-
+                        <br>
                         <li> Create document and folder specific properties / param eters that are relevant for your business and department.</li>
-
+                        <br>
                         <li> Secure and manage document / folder access and ensure compliance with security guidelines</li>
-
+                        <br>
                         <li> Enable automatic classification with metadata tagging of newly added documents</li>
-
+                        <br>
                         <li> Keep a track of various document versions through auto versioning capability</li>
                     </ul>
                 </div>
@@ -157,7 +157,7 @@
                 <div class="col-md-6 text-justify">
                     <ul style="padding-left:17px;">
                         <li>Integrate with third party business system s and applications through Web API and Web Service based integration and Custom APIs</li>
-
+                        <br>
                         <li>Leverage out of the box integration adaptors for applications such as MS Office, and Office 365</li>
 
                     </ul>
@@ -188,7 +188,7 @@
                 <div class="col-md-8 text-justify">
                     <ul style="padding-left:17px;">
                         <li>Easily retrieve and edit information in the latest or an older version of a document for enhanced research, decision making, and auditing with built in Document Version Control. This feature records a history of all new versions concerning a document for easy evaluation of information.</li>
-
+                        <br>
                         <li>Typical life cycle of a document includes everything from collaboration, improvement, review, revision and approval.</li>
 
                     </ul>
@@ -213,13 +213,13 @@
                 <div class="col-md-6 text-justify">
                     <ul style="padding-left:17px;">
                         <li>Search documents and discover information with intuitive search capabilities</li>
-
+                        <br>
                         <li>Optimize search queries by removing redundant search words and phrases</li>
-
+                        <br>
                         <li>Easily locate the searched content within a document through the searched content highlight feature</li>
-
+                        <br>
                         <li>Search with few keywords using auto suggestion feature, which works across documents, folders, index fields and their values</li>
-
+                        <br>
                         <li>Leverage strong federation capabilities</li>
                     </ul>
                 </div>
@@ -249,13 +249,13 @@
                 <div class="col-md-8 text-justify">
                     <ul style="padding-left:17px;">
                         <li>Web based user interface for easy retrieval of documents</li>
-
+                        <br>
                         <li>Secured access to content ensuring document confidentiality and integrity of records</li>
-
+                        <br>
                         <li>Geographic Information Systems (GIS) for linking, storing and retrieval of documents with the help of third party integrations</li>
-
+                        <br>
                         <li>Comprehensive reports on transacted documents</li>
-
+                        <br>
                         <li>Secure archival based on internal policies and regulations</li>
 
                     </ul>
@@ -277,13 +277,13 @@
                 <div class="col-md-6 text-justify">
                     <ul style="padding-left:17px;">
                         <li>Collaborate and verify documents collected from multiple data sources</li>
-
+                        <br>
                         <li>Automate linear workflows such as Maker Checker and Approval/ Rejection</li>
-
+                        <br>
                         <li>Maintain a central library for all the business process related documents</li>
-
+                        <br>
                         <li>Design and deploy document processes with inbuilt work steps such as upload, verify, approve, and reject</li>
-
+                        <br>
                         <li>Upload documents with easy drag and drop feature</li>
 
                     </ul>
@@ -314,13 +314,13 @@
                 <div class="col-md-8 text-justify">
                     <ul style="padding-left:17px;">
                         <li>Track, manage and share digital assets online</li>
-
+                        <br>
                         <li>Store images, videos, audios, documents, HTML, CSS, XML and other digital assets</li>
-
+                        <br>
                         <li>Compress and transform content while improving quality</li>
-
+                        <br>
                         <li>Instantly search through numerous digital assets</li>
-
+                        <br>
                         <li>Facilitate online streaming through integrated streaming engines</li>
 
 
@@ -346,15 +346,15 @@
                 <div class="col-md-6 text-justify">
                     <ul style="padding-left:17px;">
                         <li>Easy Information Access Bridge the gap between business operations and workforce.</li>
-
+                        <br>
                         <li>Enable easy access to documents amid information users</li>
-
+                        <br>
                         <li>Add documents via mobile application and upload them into central repository</li>
-
+                        <br>
                         <li>Search documents and search criterion stored in the repository</li>
-
+                        <br>
                         <li>Create and access folders in the repository through your mobile</li>
-
+                        <br>
                         <li>Capture, archive and retrieve all types of document formats, coming from various sources</li>
                     </ul>
                 </div>
@@ -384,11 +384,11 @@
                 <div class="col-md-8 text-justify">
                     <ul style="padding-left:17px;">
                         <li>Full audit trail setting allows logging of every activity happening on a file or folder</li>
-
+                        <br>
                         <li>Administrators and managers can query audit logs using various time, user and location criteria</li>
-
+                        <br>
                         <li>Helps identify steps within task process that are unnecessary, so that you can improve workflow and efficiency of your organization</li>
-
+                        <br>
                         <li>Reduces chances of manipulation of records and thwarts security breaches</li>
 
                     </ul>
