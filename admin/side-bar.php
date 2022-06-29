d7282b2a80aedb6d14915b51e8236b6a56edb247
shahanas84 <107673615+shahanas84@users.noreply.github.com>
2022-06-29 21:35:15 +0530

    123

---
+++ admin/side-bar.php
@@ -0,0 +1,54 @@
+<div class="sidebar">
+    <div class="logo-details">
+        <i class=' p-0 m-0'></i>
+        <span class="logo_name text-small">Admin</span>
+    </div>
+    <ul class="nav-links">
+        <li>
+            <a href="#" class="active">
+                <i class='bx bx-grid-alt' ></i>
+                <span class="links_name">Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class='bx bx-box' ></i>
+            <span class="links_name">Tickets</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class='bx bx-list-ul' ></i>
+            <span class="links_name">Shops</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+            <i class='bx bx-pie-chart-alt-2' ></i>
+            <span class="links_name">Exhibitions</span>
+            </a>
+        </li>
+
+
+        <li>
+            <a href="#">
+            <i class='bx bx-user' ></i>
+            <span class="links_name">Users</span>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+            <i class='bx bx-cog' ></i>
+            <span class="links_name">Settings</span>
+            </a>
+        </li>
+        
+        <li class="log_out">
+            <a href="#">
+            <i class='bx bx-log-out'></i>
+            <span class="links_name">Log out</span>
+            </a>
+        </li>
+    </ul>
+</div>
