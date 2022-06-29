f3ae719ed69129616f63659196561c93bb17625a
rakoon-cloud <88817543+rakoon-cloud@users.noreply.github.com>
2022-06-29 10:51:37 +0530

    e

--- codea_mod/document-management-system.php
+++ codea_mod/document-management-system.php
@@ -129,27 +129,15 @@
                 
                 <div class="col-md-8 text-justify">
                     <ul style="padding-left:17px;">
-                    <li>Systematically organize documents in a folder and subf older hierarchy
+                        <li>Systematically organize documents in a folder and subf older hierarchy
 
-                    <li> Create document and folder specific properties / param eters that are relevant for your business and department.
+                        <li> Create document and folder specific properties / param eters that are relevant for your business and department.
 
-                    <li> Secure and manage document / folder access and ensure compliance with security guidelines
+                        <li> Secure and manage document / folder access and ensure compliance with security guidelines
 
-                    <li> Enable automatic classification with metadata tagging of newly added documents
+                        <li> Enable automatic classification with metadata tagging of newly added documents
 
-                    <li> Keep a track of various document versions through auto versioning capability
-                    </ul>
-                </div>
-                
-                <div class="col-md-12 text-justify">
-                    <ul style="padding-left:17px;">
-                        <li>
-                        	Lightweight and secured <br>
-                            IoT devices tend to be small in form-factor that is limited RAM, disk space, low power requirement etc. This makes it important to pay great attention to resource requirement and consumption. And due to the fact, that IoT is all about scaled and smart devices deployed, we need to focus on the scalability and security of these devices.
-                        </li>
-                        <li>
-                        	We have worked with many companies that started pilot IoT projects and quickly realized that the architecture they had created would not scale to support their application. So the key is planning for the quantity of data which will be created. Building an IoT platform on high performance, massively scalable and highly reliable platform from the start can help to avoid scalability and performance challenges that arise with production scale deployments.
-                        </li>
+                        <li> Keep a track of various document versions through auto versioning capability
                     </ul>
                 </div>
                 
@@ -163,14 +151,15 @@
             <div class="row">
                 
                 <div class="col-md-12">
-                    <h1 style="text-align:left !important; margin-bottom:20px;">Security</h1>
+                    <h1 style="text-align:left !important; margin-bottom:20px;">Integration</h1>
                 </div>
                 
                 <div class="col-md-6 text-justify">
                     <ul style="padding-left:17px;">
-                    	<li><b>Reliability:</b> You need a persistent and high-speed connection that can handle any amount of data, and an uptime guarantee to maintain service to your deployment. You need reliability at scale, not just in the lab. Has to ensure that performance doesn’t fail and your infrastructure could be globally scaled so that users could be added further away from your points of presence.</li>
-                        <li><b>Security:</b> IoT devices are expensive and added to that the failure or security vulnerabilities could have a real-life impact on the people around them. You need to ensure that you’re protecting your deployment itself as well as the data flowing through it. While forming a security strategy important considerations should end to end encryption, secure firmware, access management and open-port management.</li>
-                        <li><b>Low-latency:</b> Speed is an important factor. One has to make sure they use low latency protocols and technologies to provide the real-time speed necessary to control and monitor IoT devices. Every millisecond counts when it comes to devices.</li>
+                        <li>Integrate with third party business system s and applications through Web API and Web Service based integration and Custom APIs</li>
+
+                        <li>Leverage out of the box integration adaptors for applications such as MS Office, and Office 365</li>
+
                     </ul>
                 </div>
                 
@@ -189,7 +178,7 @@
             <div class="row">
                 
                 <div class="col-md-12">
-                <h1 style="text-align:left !important; margin-bottom:20px;">Integration</h1>
+                <h1 style="text-align:left !important; margin-bottom:20px;">Version Controlling</h1>
                 </div>
                 
                 <div class="col-md-4">
@@ -198,17 +187,14 @@
                 
                 <div class="col-md-8 text-justify">
                     <ul style="padding-left:17px;">
-                    	<li>Adoption of IoT technologies has become a much-needed factor for organizations to stay ahead of their competitors and to effectively meet the demands of their customers. Organizations, therefore, need to advance their ability to adopt these technologies while minimizing the unacceptable risk. An ideal solution for organizations will be deploying IoT assets as modular pieces and by adopting a network approach of introducing them incrementally, visibly and intentionally to the current system. The assets could be exposed as APIs, which can be configured on a zero-trust model, creating layered defences that prevent the API from trusting new entrants prematurely. <br>While organizations continue to add new technologies at a faster stride, this approach is very much vital to allow them to minimize and mitigate the associated risks at the same time.</li>
-                        <li>The past two years have seen a burst of platforms like Amazon and Google Home has become more universal. There is an increasing demand for integration. Ecosystems are still in the early stages for the consumer towards a fully autonomous smart home.</li>
+                        <li>Easily retrieve and edit information in the latest or an older version of a document for enhanced research, decision making, and auditing with built in Document Version Control. This feature records a history of all new versions concerning a document for easy evaluation of information.</li>
+
+                        <li>Typical life cycle of a document includes everything from collaboration, improvement, review, revision and approval.</li>
+
                     </ul>
                 </div>
                 
-                <div class="col-md-12 text-justify">
-                    <ul style="padding-left:17px;">
-                        <li>Has to bring all parties together to avoid functional isolation. Has to involve data science from the beginning. The first step is to decide what you are trying to achieve.</li>
-                        <li>Specifications and certification are the main factors. Also has to take care of the open-source project to provide a reference implementation of the OCF specifications. Throughout the IoT pipeline focus should be on interoperability and security</li>
-                    </ul>
-                </div>
+               
                 
             </div>
             
@@ -221,26 +207,25 @@
             <div class="row">
                 
                 <div class="col-md-12">
-                    <h1 style="text-align:left !important; margin-bottom:20px;">Others</h1>
+                    <h1 style="text-align:left !important; margin-bottom:20px;">Advanced Search</h1>
                 </div>
                 
                 <div class="col-md-6 text-justify">
                     <ul style="padding-left:17px;">
-                    	<li>BNEP - Bluetooth low energy is a common protocol. Focus on your capabilities. Identify a reliable hardware partner. The device comes alive on the software size while UX and software make the difference. Hardware, cloud backend, an app on iOS and Android, all should work together.</li>
-                        <li>We work with logistics, Hospitality, Retail and healthcare. Retail and hospitality know what they want to do to create a better customer experience. While Logistics companies have an existing connected asset within the business and they want to make sure to monitor those assets so they can move to predictive maintenance.<br>We support them with regards to a)data problems, data plane b) modernizing and improving streams of data or cloud-native applications c) providing applications and services, d), deploying and managing new infrastructure and e) how to deploy across hybrid environments.</li>
-                        <li>One should have a clear understanding of the system you are developing on. Key to adoption is to create industry-specific solutions.</li>
+                        <li>Search documents and discover information with intuitive search capabilities</li>
+
+                        <li>Optimize search queries by removing redundant search words and phrases</li>
+
+                        <li>Easily locate the searched content within a document through the searched content highlight feature</li>
+
+                        <li>Search with few keywords using auto suggestion feature, which works across documents, folders, index fields and their values</li>
+
+                        <li>Leverage strong federation capabilities</li>
                     </ul>
                 </div>
                 
                 <div class="col-md-6">
                     <img src="assets/images/Iot-key-factors-img-05.png" alt="" class="img-fluid img-border" />
-                </div>
-                
-                <div class="col-md-12 mt-3 text-justify">
-                    <ul style="padding-left:17px;">
-                        <li>The fundamentals are similar to cloud software development. You need a good software development process for testing, working with a team, deploying across a lot of systems.</li>
-                        <li>Has to keep your feet frim on the ground even when your head is in the cloud. In other words, sticking to the actual needs of your customer in mind is critical while you keep up with the latest technologies, trends, and offerings in the IoT space. Be the bridge between what your customers truly need now and the use of devices / cutting-edge technologies. Dissatisfaction starts popping up when you address a challenge with an innovative technology solution without properly addressing an existing problem.</li>
-                    </ul>
                 </div>
                 
             </div>    
