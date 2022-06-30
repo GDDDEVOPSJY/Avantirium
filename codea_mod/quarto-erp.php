85421013e4c544ccf40d38cabb5d6cea72ef8469
CodeaInterns <107670562+CodeaInterns@users.noreply.github.com>
2022-06-30 10:14:26 +0530

    h

---
+++ codea_mod/quarto-erp.php
@@ -0,0 +1,212 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Custom software development company Long Island | Software development Long Island</title>
+
+    <meta name="description" content="Having a great software idea can be very exciting.Finding the perfect custom software development company phoenix,island to help you design your dream software.Codea technologies assure best software development in long island">
+    <link rel="shortcut icon" href="assets/images/codea-icon.png" type="ima">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <script src="https://kit.fontawesome.com/085049fa59.js"></script>
+    <link rel="canonical" href="https://www.codeatechnologies.com/software-development">
+
+    <style>
+        .item-four .nav-link {
+            border-top: 4px solid #f1585d;
+            padding: 1.68rem 1rem;
+            background: rgba(0, 0, 0, 0.05);
+        }
+    </style>
+
+</head>
+
+<body>
+
+    <?php include('header.php'); ?>
+
+    <section class="sd-banner">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-6">
+                    <div class="sd-head">
+
+                        <!-- <h2>PRODUCT ENGINEERING</h2> -->
+                        <h1>Quarto ERP</h1>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="mt-5 mb-5">
+        <div class="container text-justify">
+            <div class="row">
+                <h1 class="pb-3 m-5 text-center">
+                    A One Stop Solution for Distribution, Retail and Service Companies
+                </h1>
+                <p class="bold">
+                    Quarto ERP is a complete ERP solution that helps the Small and Medium
+                    Businesses to operate efficiently for achieving prosperity. Quarto ERP is
+                    capable of adapting and implementing the management systems in place for
+                    each of the departments. Production, Inventory Control, Purchase,
+                    Import, Sales, Export, Distribution, Finance, CRM, HRM, Document
+                    Management Systems work seamlessly in cloud Server with all the three
+                    computing platform variants that is Web application (Browser based),
+                    Desktop Software (Windows based) and Android Applications(Mobile).
+                    Quarto ERP is a best pick for Small or Medium Manufacturers and
+                    Distributors who have multiple branches orwarehouses.
+                </p>
+                <p class="text-center" style="width: 60%; margin: 30px 20% 20px 20%;">
+                    The product has supported businesses in India, Middle East and Africa for
+                    nearly two decades and keeps improving the capabilities and extending to
+                    newer technologies.
+                </p>
+            </div>
+            <div class="row">
+                <div class="col-4 mt-4">
+                    <img src="assets/images/erp-img.jpg" style="width: 100%; height: auto;">
+                </div>
+                <div class="text-justify col-8 p-0 mt-4">
+                    <p class="col-12 p-0 m-0 mt-3">
+                        One of the key features of Quarto ERP is its Flexibility in implementing
+                        different business practices. Implement the business workflows/logics,
+                        Documents design, Reporting features, User control with their hierarchy
+                        based rights etc. by configuration capabilities reduce the immense coding
+                        requirements and eliminate further complexities in managing the application.
+                    </p>
+                    <p class="col-12 p-0 m-0 mt-3">
+                        Quarto ERP is a quick, stable and feature filled software solution that helps
+                        and takes care of the organization for a longer period of time.
+                        Implementation of the product is also comparatively speedy and obviously a
+                        very cost effective solution in implementation and ownership.
+                    </p>
+                </div>
+            </div>
+            <div class="list-of-contents col-12 p-0">
+                <h1 class="heading mt-5">List of Contents</h2>
+                    <div class="row">
+                        <ul class="col-4 col-sm-5 mt-4 ml-4">
+                            <li><a onclick="goToId('Administration')">ADMINISTRATION</a></li>
+                            <li><a href="#Purchase">PURCHASE</a></li>
+                            <li><a href="#Production">PRODUCTION</a></li>
+                            <li><a href="#Sales">SALES</a></li>
+                            <li><a href="#Inventory">INVENTORY</a></li>
+                            <li><a href="#Financial%20Accounting">FINANCIAL ACCOUNTING</a></li>
+                            <li><a href="#Budget">BUDGET</a></li>
+                            <li><a href="#Costing">COSTING</a></li>
+                            <li><a href="#Duties & Taxes">DUTIES & TAXES</a></li>
+                            <li><a href="#Payroll">PAYROLL</a></li>
+                        </ul>
+                        <ul class="col-4 col-sm-5 mt-4">
+                            <li><a href="#Communication">COMMUNICATION</a></li>
+                            <li><a href="#CRM">CRM</a></li>
+                            <li><a href="#Asset%20Management">ASSET MANAGEMENT</a></li>
+                            <li><a href="#General%20Features">GENERAL FEATURES</a></li>
+                            <li><a href="#Quarto%20Mobile">QUARTO MOBILE</a></li>
+                            <li><a href="#Quarto%20E-Commerce">QUARTO E-COMMERCE</a></li>
+                            <li><a href="#Document%20Management%20System">DOCUMENT MANAGEMENT SYSTEM</a></li>
+                            <li><a href="Branch%20Management">BRANCH MANAGEMENT</a></li>
+                            <li><a href="#OfflineOnline%20Cloud%20Synchronisation">OFFLINE/ONLINE CLOUD SYNCHRONISATION</a></li>
+                        </ul>
+                    </div>
+                    <script src="http://flesler.com/jquery/scrollTo/js/jquery.scrollTo-min.js">
+                        function goToId (id) {
+                            $('a').scrollTo( $(id), 1000, {offset:-100} );
+                        }
+                    </script>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="mb-5" id="Administration">
+        <div class="container">
+            <h1 class="pb-3 mb-2 text-center">ADMINISTRATION</h1>
+            <div class="row">
+                <!-- <div class="image col-4">
+                    <img src="assets/images/" class="col-12 p-0">
+                </div> -->
+                <div class="col-4">
+                    <ul type=circle>
+                        <li>Company HO & Branch Management</li>
+                        <li>HO & Branch Offline Data Synchronization</li>
+                        <li>User Management</li>
+                        <li>Configure Security Features</li>
+                        <li>Schedule Database Backup System</li>
+                        <li>Import/Export Backup data</li>
+                        <li>SMS/Email Schedule</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <ul type=circle>
+                        <li>Print/Reprint Options</li>
+                        <li>Multiple Financial years</li>
+                        <li>Formula Builder for configuration</li>
+                        <li>Design Entry screens</li>
+                        <li>Dynamic Locking System</li>
+                        <li>Print Format of Vouchers/Bills</li>
+                        <li>Configure/Create Reports</li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <i>Setup Peripherals (Optional)</i>
+                    <ul type=disc>
+                        <li>Printer (Laser/Inkjet/Thermal/Dot-matrix)</li>
+                        <li>Barcode Printer</li>
+                        <li>Barcode Scanner</li>
+                        <li>Cash Drawer</li>
+                        <li> Display Pole</li>
+                        <li>Weighing Machine</li>
+                        <li>POS Machine with Touch Screen</li>
+                        <li>Biometrics (Attendance Punching)</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="mb-5" id="Purchase">
+        <div class="container">
+            <h1 class="pb-3 mb-2 text-center">PURCHASE</h1>
+            <div class="row">
+                <div class="col-4">
+                    <ul type=circle>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <ul type=circle>
+                        <li></li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <i>Setup Peripherals (Optional)</i>
+                    <ul type=disc>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include('footer.php'); ?>
+
+    <!-- JAVASCRIPT -->
+    <script src="assets/js/jquery-3.3.1.slim.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.js" type="text/javascript"></script>
+
+    <script src="assets/js/menu.js" type="text/javascript"></script>
+
+</body>
+
+</html>
