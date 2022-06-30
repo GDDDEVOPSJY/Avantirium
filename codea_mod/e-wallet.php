a933bbfb0ec9c3cb336f2609a4cdd93450fd974e
rakoon-cloud <88817543+rakoon-cloud@users.noreply.github.com>
2022-06-30 11:08:37 +0530

    f

--- codea_mod/e-wallet.php
+++ codea_mod/e-wallet.php
@@ -40,6 +40,177 @@
         </section>
 
 
+
+
+
+
+        <section class="text-center pt-5 pb-4">
+        <div class="container">
+        	
+            <h1>OUR MODULES</h1>
+            <h2 class="pt-3 codea-color2">Full bouquet to Acquire Service and Engage customers</h2>
+           
+            
+            <div class="row text-center">
+            	   
+        	    <div class="col-md-12 col-sm-12 mt-2">
+                	<img src="assets/images/ewallet-li.png" alt="PMIS solution Architecture" style="height: auto; width: 100%;"/>
+                </div>
+                
+                
+                
+            </div>
+            
+        </div>
+
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <section class="pt-3 pb-3" style="background:#e5e5e5;">
+        <div class="container">
+        	
+            <div class="mt-3 text-center">
+            	<h1 class="black" >Digital Wallet Platform</h1>
+                <h2 class="mt-2 black">
+                A white labelled ready to use platform with certified security and uptime guarantee
+                </h2>
+            </div>
+            
+            <div class="row mt-5">
+            	
+                <div class=" black col-md-3 mb-2">
+                	
+                    <ul class="mt-3 text-left">
+                        <li>Omni Channel experience</li>
+                        <li>Digital Onboarding & eKYC</li>
+                        <li>Banking as a Service</li>
+                        <li>Peer to Peer</li>
+                        <li>Peer to merchant</li>
+                    </ul>
+                </div>
+                
+                <div class=" black col-md-3  mb-2">
+                
+                    <ul class="mt-3 text-left">
+                        <li>Card Management</li>
+                        <li>International remittance</li>
+                        <li>Bil Payments</li>
+                        <li>Micro lending</li>
+                        <li>Loyalty</li>
+                    </ul>
+                </div>
+
+                <div class=" black col-md-3  mb-2">
+                	
+                    <ul class="mt-3 text-left">
+                        <li>Sanction screening</li>
+                        <li>Sub wallets</li>
+                        <li>Chargeback Management</li>
+                        <li>Collections</li>
+                        <li>Goal Setting</li>
+                    </ul>
+                </div>
+
+                <div class=" black col-md-3  mb-2">
+                	
+                    <ul class="mt-3 text-left">
+                        <li>Payment to unregistered client</li>
+                        <li>Invoicing</li>
+                        <li>Spending limits</li>
+                        <li>Transaction monitoring and alerts</li>
+                        <li>Split payments</li>
+                    </ul>
+                </div>
+                
+                
+               
+                
+            </div>
+            
+        </div>
+    </section>
+
+
+
+
+
+
+    <section class="ewal-safe pt-3 pb-3">
+        <div class="container">
+        <div class="row">
+        	
+           
+            
+            <div class="col-md-6">
+            	
+                <div class="ewal-safe-content ">
+                	
+                    <h1>The Safe Platform</h1>
+
+                    
+                    
+                    <p class="font-weight-normal white">
+                    The Safe platform is built in collaboration by software engineers, business analysts, professionals from the financial services sector, and pioneers of digital financial services.
+                    </p>
+
+                    <p>
+                    The core is built on Java with a GWT front end and uses the SPRING framework to create secure interfaces with other components.
+                    </p>
+
+                    <p>
+                    Interoperability on the platform is provided by Web APIs, or remoting over HTTPS for native Java clients.
+                    </p>
+
+                    <p>
+                    Services in other languages can access Safe over an API using JSON messages over HTTPS.
+                    </p>
+
+                    <p>
+                    Safe also has a RestFUL API compatible to OpenAPI specifications, enabling it to interface with almost all 3 rd party servers.
+                    </p>
+                    
+                </div>
+                
+            </div>
+
+            <div class="col-md-6">
+                <img src="assets/images/ewallet-dashboard.png" alt="" class="img-fluid" />
+            </div>
+            
+        </div>
+        </div>
+        </section>
+
+
+
+
+
+
+
+
    
         <section class="ewallet-busi pt-5 pb-5">
             <div class="container">
@@ -82,16 +253,6 @@
             </div>
             </section>
 
-      
-
-
-
-
-
-
-
-
-
             <section class="e-quality mt-5 mb-5">
         <div class="container">
             
@@ -131,42 +292,224 @@
                         <li class="text-left" style="padding:5px; background:rgba(0,0,0,0.1);">Choose the modules you need - add more when required</li><br>
                         <li class="text-left" style="padding:5px; background:rgba(0,0,0,0.1);">Fully managed solution - Zero investment on Hardware</li><br>
                         <li class="text-left mb-2" style="padding:5px; background:rgba(0,0,0,0.1);">Pay as you grow model</li>
-                    
                   
                 </div>
-
-                
-              
-             
-
-                
-               
-
-                   
-                
+  
             </div>	
             
         </div>
+      	   
+        </section>
+        
 
-       
-            	   
+        <section class="ewal-hd">
+
+<div class="container">
+    
+        <div class="row mt-0">
+            
+            <div class="col-md-12 mt-4 mb-4">
+               
+                <h1 class="mb-2 white text-center">
+                EMPOWERING FLEXIBILITY
+                </h1>
+                <p class="white text-justify mb-1 text-center">    
+                Platform is designed to enable business users to innovate by empowering them with flexibility
+                </p>
+                
+            </div>
+            
+        </div>
+
+</section>
+
+        <section class="mt-5 mb-5">
+        <div class="container">
+           
+        	
+            <div class="row">
+            	
+                <div class="col-md-6">
+                	
+                    <h1 class="mt-5 mb-3"><span class="codea-color1">Unlimited User Types</span></h1>
+                    <ul>
+                        <li>Create user groups based on customer profiles:
+                        Demographics, QoS , KYC, Organizational Role, Etc.</li>
+                        <li>Offer Group specific services: Discounts, promotions, loyalty programs, etc.</li>
+                        <li>Focused BI & Reporting: Sharpen your analytics with highly focused data sets and reports.</li>
+                    </ul>
+                    
+                </div>
+                
+                <div class="col-md-6 text-center">
+                	
+                    <img src="assets/images/svg/unlimited_user.svg" alt="unlimited user" class="img-fluid perspective-round-img mt-5" />
+                    
+                </div>
+                
+            </div>
+            
+            <hr class="mt-5 mb-3" style="border-bottom:dashed 1px rgba(159,159,159,0.3);" />
+            
+             <div class="row">
+            	
+                <div class="col-md-6 text-center">
+                	
+                    <img src="assets/images/svg/unlimited_products.svg" alt="Unlimited Products" class="img-fluid perspective-round-img mt-5" />
+                    
+                </div>
+                
+                <div class="col-md-6">
+                	
+                    <h1 class="mt-3 mb-3"><span class="codea-color2">Unlimited Products</span></h1>
+                    <ul>
+                        <li>Codea provides one of the most customizable product creations suites in the world.</li>
+                        <li>Wallets with interest functions like savings, deposits, loans, etc.</li>
+                        <li>Multi currency support for cross border payments.</li>
+                        <li>Rule based loyalty programs.</li>
+                    </ul>    
+                    
+                </div>
+                
+            </div>
+            
+            <hr class="mt-5 mb-3" style="border-bottom:dashed 1px rgba(159,159,159,0.3);" />
+            
+            <div class="row">
+            	
+                <div class="col-md-6">
+                	
+                    <h1 class="mt-3 mb-3"><span class="codea-color1">Lifecycle Management</span></h1>
+                    <ul>
+                        <li>Workflow based user registration activation suspension management</li>
+                        <li> Rapid onboarding using social log in, biometrics, and ID.</li>
+                        <li> BI tools to identify behavior patterns and initiate proactive measures.</li>
+                        <li>Seamless group transitions and status management.</li>	
+                    </ul>
+                </div>
+                
+                <div class="col-md-6 text-center">
+                	
+                    <img src="assets/images/svg/lifecycle_management.svg" alt="Lifecycle Management" class="img-fluid perspective-round-img mt-5" />
+                    
+                </div>
+                
+            </div>
+            
+            <hr class="mt-5 mb-3" style="border-bottom:dashed 1px rgba(159,159,159,0.3);" />
+            
+             <div class="row">
+            	
+                <div class="col-md-6 text-center">
+                	
+                    <img src="assets/images/svg/api_based.svg" alt="Api Based Services" class="img-fluid perspective-round-img" />
+                    
+                </div>
+                
+                <div class="col-md-6">
+                	
+                    <h1 class="mt-3 mb-3"><span class="codea-color2">API Based Services</span></h1>
+                    <ul>
+                        <li>Access global services via the Safe API.</li>
+                        <li>Keep your services relevant and up to date.</li>
+                        <li>Add new services with minimal go to market lags.</li>
+                        <li>Create your own services for 3 rd parties.</li>
+                    </ul>
+        
+                   
+                    
+                </div>
+                
+            </div>
+            
+        </div>
         </section>
 
+        <section class="text-center pt-5 pb-5" style="background-color: #363536;">
+        <div class="container">
+        	
+            <h1 class="white">Service Enhancements</h1>
+            
+            <div class="row">
+            	
+               
+                
+                <div class="col-md-3 mt-5">
+                <div class="ewal-item">
+                	
+                    <div class="cs-img" style="background-image:url(assets/images/ewallet-enh-1.png);"></div>
+                    
+                    <div class="ewal-safe-content pt-2 ">
+                    	
+                       
+                        <p class="pt-2">
+                        Codea provides native support for NFC tokenization and authentication. it is interoperable with all hardware NFC that follow the ISO protocols.
+                        </p>
+                        
+                        
+                    </div>
+                    
+                </div>
+                </div>
 
+                <div class="col-md-3 mt-5">
+                <div class="ewal-item">
+                	
+                    <div class="ewal-img" style="background-image:url(assets/images/ewallet-enh-2.png);"></div>
+                    
+                    <div class="ewal-safe-content pt-2">
+                    	
+                      
+                        <p class="pt-2">
+                        The platform comes built with advanced financial product modelling tools like interest bearing accounts, loans and mortgages, overdrafts, group loans, and many more.
+                        </p>
+                             
+                    </div>
+                    
+                </div>
+                </div>
 
+                <div class="col-md-3 mt-5">
+                <div class="ewal-item">
+                	
+                    <div class="ewal-img" style="background-image:url(assets/images/ewallet-enh-3.png);"></div>
+                    
+                    <div class="ewal-safe-content pt-2">
+                    	
+                        <h2>QR Code Scan &amp; Pay</h2>
+                        <p>
+                        The platform provides support for dynamically generated QR codes ensuring security with a wide range of merchant applications.
+                        </p>
+                        
+                    </div>
+                    
+                </div>
+                </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
+                <div class="col-md-3 mt-5">
+                <div class="ewal-item">
+                	
+                    <div class="ewal-img" style="background-image:url(assets/images/ewallet-enh-4.png);"></div>
+                    
+                    <div class="ewal-safe-content pt-2">
+                    	
+                        <h2>EFT POS Support</h2>
+                        <p>
+                        The platform comes with a wide array of features and functions to manage service peripheral devices like POS, mobile phones, tablets, etc. The platform provides entire inventory management for all business devices.
+                        </p>
+                    
+                        
+                    </div>
+                    
+                </div>
+                </div>
+                
+               
+              
+            </div>
+            
+        </div>
+        </section>
 
         <?php include('footer.php'); ?>
         
