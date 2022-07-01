60267a3a4be9a0c68591401b92c04cc2f701e3ee
shahanas84 <107673615+shahanas84@users.noreply.github.com>
2022-07-01 11:48:57 +0530

    eee

--- av/admin/side-bar.php
+++ av/admin/side-bar.php
@@ -5,7 +5,7 @@
     </div>
     <ul class="nav-links">
         <li>
-            <a href="#" class="active">
+            <a href="index.php" class="active">
                 <i class='bx bx-grid-alt' ></i>
                 <span class="links_name">Dashboard</span>
             </a>
