0ecdbfc7d6cb06e90b64f2e499824f475a9656dd
CodeaInterns <107670562+CodeaInterns@users.noreply.github.com>
2022-07-07 15:25:17 +0530

    nm

--- arch-angles/index.php
+++ arch-angles/index.php
@@ -24,13 +24,13 @@
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img class="d-block w-100" src="images/logo1.png" alt="First slide">
+                            <img class="d-block w-100" src="images/logo.png" alt="First slide">
                         </div>
                         <div class="carousel-item">
                             <img class="d-block w-100" src="images/logo1.png" alt="Second slide">
                         </div>
                         <div class="carousel-item">
-                            <img class="d-block w-100" src="images/logo1.png" alt="Third slide">
+                            <img class="d-block w-100" src="images/logo.png" alt="Third slide">
                         </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
