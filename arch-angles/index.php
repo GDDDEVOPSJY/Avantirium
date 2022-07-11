2a9d54400ed4d82f5324da0e40c1c59f3f7c581a
CodeaInterns <107670562+CodeaInterns@users.noreply.github.com>
2022-07-11 11:13:58 +0530

    c

--- arch-angles/index.php
+++ arch-angles/index.php
@@ -12,7 +12,8 @@
         <?php include("./partials/nav-header.php"); ?>
 
         <section id="home">
-            <div class="container">
+
+            <div class="">
                 <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
                     <ol class="carousel-indicators">
                         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
@@ -29,136 +30,77 @@
                         <div class="carousel-item">
                             <img class="d-block w-100" src="images/logo.png" alt="Third slide">
                         </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    </div> <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
                         <div class="carousel-icons">
                             <span class="carousel-control-prev-icon" aria-hidden="false"></span>
                         </div>
                         <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    </a> <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
                         <div class="carousel-icons">
                             <span class="carousel-control-next-icon" aria-hidden="false"></span>
                         </div>
                         <span class="sr-only">Next</span>
                     </a>
                 </div>
+
             </div>
         </section>
 
         <section id="about">
-            <h1 class="main-head"> ABOUT US</h1>
-            <p>At Primo Consulting & Contracting, we are passionate of anything that takes to make an
-                Architectural Building. Primo's flexibility to mobilize the right people to deliver unique
-                construction solutions; an unwavering focus on delivering value to your business bottom line;
-                and the capability to leverage innovation to meet emerging challenges and keep you at the fore
-                are its core.</p>
-            <h4>Commitment stands with Quality</h4>
-            <p>As a contracting company, we provide the best quality of services
-                that starts from design to material to execution.Primo's superior and
-                experienced network of resources helps complete challenging projects
-                well within timeframes and expectations.Most importantly, our clients
-                are of highest priority. Obviously, all projects are meticulously
-                planned and monitored enroute the journey that we ensure and derives
-                pleasure and delight for the investor. This is Primo Consulting and
-                Contracting.</p>
+            <div class="container">
+                <h1 class="main-head"> ABOUT US</h1>
+                <p>At Primo Consulting & Contracting, we are passionate of anything that takes to make an Architectural Building. Primo's flexibility to mobilize the right people to deliver unique construction solutions; an unwavering focus on delivering value to your business bottom line; and the capability to leverage innovation to meet emerging challenges and keep you at the fore are its core.</p>
+                <h4>Commitment stands with Quality</h4>
+                <p>As a contracting company, we provide the best quality of services that starts from design to material to execution.Primo's superior and experienced network of resources helps complete challenging projects well within timeframes and expectations.Most importantly, our clients are of highest priority. Obviously, all projects are meticulously planned and monitored enroute the journey that we ensure and derives pleasure and delight for the investor. This is Primo Consulting and Contracting.</p>
+            </div>
         </section>
 
         <section id="services">
-            <h1 class="main-head"> OUR SERVICES</h1>
-            <h2>Architectural Building & Civil Construction</h2>
-            <p>Primo CC believes that, Excellent structures are an important
-                part of our quality of life. While, high quality structures mark a
-                positive contribution to the culture of the building and also
-                satisfies ourselves. Architectural Buildings are specialised works
-                with detailing and large quantums of planning. Execution of work with
-                specific materials are always a challenge and needs experience. We
-                have a team of highly skilled professionals backed by equipments to
-                execute the project in a tight timelines.</P>
-            <a href="#">Learn more</a>
+            <div class="container">
+                <h1 class="main-head"> OUR SERVICES</h1>
 
+                <h2>Architectural Building & Civil Construction</h2>
+                <p>Primo CC believes that, Excellent structures are an important part of our quality of life. While, high quality structures mark a positive contribution to the culture of the building and also satisfies ourselves. Architectural Buildings are specialised works with detailing and large quantums of planning. Execution of work with specific materials are always a challenge and needs experience. We have a team of highly skilled professionals backed by equipments to execute the project in a tight timelines.</p>
+                <a href="#">Learn more</a>
 
-            <h2>Pre-Engineered Buildings</h2>
-            <p>PEB is relatively a new concept in India however with the technological
-                advancement, escalating labour cost and un-certainities, PEB has become
-                quite a convenient and long term option in the building industry. These
-                buildings are cost effective and helps an investor to with fast returns
-                on investment in the least possible time.</P>
-            <a href="#">Learn more</a>
+                <h2>Pre-Engineered Buildings</h2>
+                <p>PEB is relatively a new concept in India however with the technological advancement, escalating labour cost and un-certainities, PEB has become quite a convenient and long term option in the building industry. These buildings are cost effective and helps an investor to with fast returns on investment in the least possible time.</p>
+                <a href="#">Learn more</a>
 
+                <h2>Glass Facade & Exterior Solutions</h2>
+                <p>PEB is relatively a new concept in India however with the technological advancement, escalating labour cost and un-certainities, PEB has become quite a convenient and long term option in the building industry. These buildings are cost effective and helps an investor to with fast returns on investment in the least possible time.</p>
+                <a href="#">Learn more</a>
 
-            <h2>Glass Facade & Exterior Solutions</h2>
-            <p>PEB is relatively a new concept in India however with the technological
-                advancement, escalating labour cost and un-certainities, PEB has become
-                quite a convenient and long term option in the building industry. These
-                buildings are cost effective and helps an investor to with fast returns
-                on investment in the least possible time.</P>
-            <a href="#">Learn more</a>
+                <h2>Interiors & Space Management</h2>
+                <p>PEB is relatively a new concept in India however with the technological advancement, escalating labour cost and un-certainities, PEB has become quite a convenient and long term option in the building industry. These buildings are cost effective and helps an investor to with fast returns on investment in the least possible time.</p>
+                <a href="#">Learn more</a>
 
-            <h2>Interiors & Space Management</h2>
-            <p>PEB is relatively a new concept in India however with the technological
-                advancement, escalating labour cost and un-certainities, PEB has become
-                quite a convenient and long term option in the building industry. These
-                buildings are cost effective and helps an investor to with fast returns
-                on investment in the least possible time.</P>
-            <a href="#">Learn more</a>
+                <h2>Lighting Solutions</h2>
+                <p>PEB is relatively a new concept in India however with the technological advancement, escalating labour cost and un-certainities, PEB has become quite a convenient and long term option in the building industry. These buildings are cost effective and helps an investor to with fast returns on investment in the least possible time.</p>
+                <a href="#">Learn more</a>
 
+                <h2>Indoor Walls & Cladding Solution</h2>
+                <p>PEB is relatively a new concept in India however with the technological advancement, escalating labour cost and un-certainities, PEB has become quite a convenient and long term option in the building industry. These buildings are cost effective and helps an investor to with fast returns on investment in the least possible time.</P>
+                <a href="#">Learn more</a>
 
-            <h2>Lighting Solutions</h2>
-            <p>PEB is relatively a new concept in India however with the technological
-                advancement, escalating labour cost and un-certainities, PEB has become
-                quite a convenient and long term option in the building industry. These
-                buildings are cost effective and helps an investor to with fast returns
-                on investment in the least possible time.</P>
-            <a href="#">Learn more</a>
+                <h2>Flooring Solutions</h2>
+                <p>PEB is relatively a new concept in India however with the technological advancement, escalating labour cost and un-certainities, PEB has become quite a convenient and long term option in the building industry. These buildings are cost effective and helps an investor to with fast returns on investment in the least possible time.</p>
+                <a href="#">Learn more</a>
 
-
-            <h2>Indoor Walls & Cladding Solution</h2>
-            <p>PEB is relatively a new concept in India however with the technological
-                advancement, escalating labour cost and un-certainities, PEB has become
-                quite a convenient and long term option in the building industry. These
-                buildings are cost effective and helps an investor to with fast returns
-                on investment in the least possible time.</P>
-            <a href="#">Learn more</a>
-
-            <h2>Flooring Solutions</h2>
-            <p>PEB is relatively a new concept in India however with the technological
-                advancement, escalating labour cost and un-certainities, PEB has become
-                quite a convenient and long term option in the building industry. These
-                buildings are cost effective and helps an investor to with fast returns
-                on investment in the least possible time.</P>
-            <a href="#">Learn more</a>
-
-
-            <h2>Architecture & Design Solutions</h2>
-            <p>PEB is relatively a new concept in India however with the technological
-                advancement, escalating labour cost and un-certainities, PEB has become
-                quite a convenient and long term option in the building industry. These
-                buildings are cost effective and helps an investor to with fast returns
-                on investment in the least possible time.</P>
-            <a href="#">Learn more</a>
+                <h2>Architecture & Design Solutions</h2>
+                <p>PEB is relatively a new concept in India however with the technological advancement, escalating labour cost and un-certainities, PEB has become quite a convenient and long term option in the building industry. These buildings are cost effective and helps an investor to with fast returns on investment in the least possible time.</p>
+                <a href="#">Learn more</a>
+            </div>
 
         </section>
 
         <section id="why-us">
-            <h1> WE THINK AND WORK FOR YOU </h1>
-            <h2> Design, Procure & Build</h2>
-            <p>Our Design Team is a young experienced and enthusiastic professional team of
-                architects who have worked on large and medium sized projects. The work comprises
-                of Commercial, Industrial, Residential and Hospitality projects. We work from the
-                concept stage of visualising the client requirement and suitably detailing it.
-                We categorise our architectural and designing scope for the complete structure to
-                the most minute detail for your interiors. We are well versed with the Building
-                rules as notified by the state governments.Procurement plays the most important
-                aspect of designing a project, cost & budget and timely availability onsite.
-                We source material from various countries depending on the project needs.
-                Products from Indian states are vastly used and recommended in our projects.
-                We directly work with the manufacturers to obtain best competitive rates.
-                Usually our projects have custom built solutions and hence an eye-for-detailing
-                and procurement is a must. We at Primo Consulting & Contracting are enthusiastic
-                in creating specialised buildings and structures that are distinct in design in
-                complex and specific architecture.
 
+            <div class="container">
+                <h1> WE THINK AND WORK FOR YOU </h1>
+                <h2> Design, Procure & Build</h2>
+                <p>Our Design Team is a young experienced and enthusiastic professional team of architects who have worked on large and medium sized projects. The work comprises of Commercial, Industrial, Residential and Hospitality projects. We work from the concept stage of visualising the client requirement and suitably detailing it. We categorise our architectural and designing scope for the complete structure to the most minute detail for your interiors. We are well versed with the Building rules as notified by the state governments.Procurement plays the most important aspect of designing a project, cost & budget and timely availability onsite. We source material from various countries depending on the project needs. Products from Indian states are vastly used and recommended in our projects. We directly work with the manufacturers to obtain best competitive rates. Usually our projects have custom built solutions and hence an eye-for-detailing and procurement is a must. We at Primo Consulting & Contracting are enthusiastic in creating specialised buildings and structures that are distinct in design in complex and specific architecture.</p>
+            </div>
         </section>
 
         <!-- <section id="gallery">
@@ -170,22 +112,18 @@
         </section> -->
 
         <section id="build-wu">
-            <div class="picture">
+            <div class="picture container">
                 <h1>Ready to work together?</h1>
-                <p>Our interests, profiles and credentials with a very clear
-                    understanding for deliverables <br>to clients will surely make you
-                    consider us. Let's discuss and find an opportunity. <br>
-                    This is Primo Consulting & Contracting.</p>
-                <button>bulid with us</button>
+                <p>Our interests, profiles and credentials with a very clear understanding for deliverables
+                    <br>to clients will surely make you consider us. Let's discuss and find an opportunity.
+                    <br>This is Primo Consulting & Contracting.
+                </p>
+                <button>Bulid with us</button>
             </div>
-
         </section>
-
         <!-- FOOTER -->
         <!-- <?php include("./partials/footer.php"); ?> -->
-
     </div>
-
     <!-- ESSENTIAL SCRIPTS -->
     <?php include("./partials/scripts.php"); ?>
 </body>
